f79f223002ed077d60838ee87b1a0dd79a9915e9
David Conner <rdconner@us.ibm.com>
2017-03-17 16:17:39 -0500

    added default branch and use branch to complete string literal

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 const fs = require('fs');
 const path = require('path');
 const regEx = /ref: refs\/heads\/([^\n]+)/;
-const gitBranch = function(branch) {
+const gitBranch = function(branch = publish) {
   console.log(branch);
   fs.readFile(`${path.join(process.cwd(), '.git/HEAD')}`, 'utf8', (err, data) => {
     //if error in getting
@@ -13,13 +13,12 @@ const gitBranch = function(branch) {
     const match = regEx.exec(data);
     if (match) {
       if (match[1] === branch) {
-        console.log('Shit is legit yoooooo', match[1]);
-        fs.readFile(path.join(process.cwd(),'gitignores/gitignore_publish.txt'), 'utf8', (err, data) => {
+        fs.readFile(path.join(process.cwd(), `gitignores/gitignore_${branch}.txt`), 'utf8', (err, data) => {
           fs.writeFile('./.gitignore', data, (err) => {
             if (err) throw err;
-            console.log('It\'s saved!');
+            console.log('Something is went wrong!!');
           });
-          console.log('DATA', data);
+          console.log('New .gitignore created: ', data);
         });
       } else {
         fs.readFile(path.join(process.cwd(),'gitignores/gitignore_master.txt'), 'utf8', (err, data) => {
